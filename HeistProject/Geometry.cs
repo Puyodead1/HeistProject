916a056946d067b2e3f86ed7167495e73769b5e8
Puyodead1 <14828766+Puyodead1@users.noreply.github.com>
2022-04-06 08:32:11 -0400

    [ci skip] Update decompiler comments

--- HeistProject/Geometry.cs
+++ HeistProject/Geometry.cs
@@ -1,8 +1,4 @@
 ﻿// Decompiled with JetBrains decompiler
-// Type: HeistProject.Geometry
-// Assembly: HeistProject, Version=0.4.32.678, Culture=neutral, PublicKeyToken=null
-// MVID: 41C549D2-321F-410C-95E1-71855CBC3D16
-// Assembly location: C:\Users\23562\Downloads\HeistProject.dll
 
 using GTA;
 using GTA.Math;
